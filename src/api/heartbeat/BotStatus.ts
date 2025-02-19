f87a12b6b6273e92517ffb2af4c438aec5e7941b
poker-cats-co <hasiradoo@starowl.dev>
2025-02-19 11:57:01 +0100

    fix status api

--- src/api/heartbeat/BotStatus.ts
+++ src/api/heartbeat/BotStatus.ts
@@ -3,24 +3,26 @@ import { bunnyLog } from 'bunny-log'
 
 /**
  * Checks the status of the bot and the database connection.
- * @returns {Promise<{ bot_status: 'online' | 'offline', db_status: { status: string, error?: string } }>} The status of the bot and the database connection.
+ * @returns {Promise<{ bot_status: 'working' | 'not_working' | 'disabled' | 'booting_up', db_status: { status: string, error?: string } }>} The status of the bot and the database connection.
  */
 const checkHeartbeat = async (): Promise<{
-	bot_status: 'online' | 'offline'
+	bot_status: 'working' | 'not_working' | 'disabled' | 'booting_up'
 	db_status: { status: string; error?: string }
 }> => {
-	const botStatusPromise = new Promise<'online' | 'offline'>((resolve) => {
-		if (!client.isReady()) return resolve('offline')
-		resolve('online')
+	const botStatusPromise = new Promise<
+		'working' | 'not_working' | 'disabled' | 'booting_up'
+	>((resolve) => {
+		if (!client.isReady()) return resolve('booting_up')
+		resolve('working')
 	})
 
 	const dbStatusPromise = new Promise<{ status: string; error?: string }>(
 		(resolve) => {
 			try {
-				resolve({ status: 'online' })
+				resolve({ status: 'working' })
 			} catch (error) {
 				bunnyLog.error('Database connection error:', error)
-				resolve({ status: 'offline', error: error.message })
+				resolve({ status: 'not_working', error: error.message })
 			}
 		}
 	)
