67beaae416a2026f494cee6613a113cff2b4f0d3
poker-cats-co <hasiradoo@starowl.dev>
2025-05-05 18:18:02 +0200

    removed unused code

--- deploy-commands.ts
+++
@@ -1,9 +0,0 @@
-const contextMenuCommands = [
-	require('./src/commands/moderation/purgeContext').data,
-]
-
-const slashCommands = [require('./src/commands/moderation/clear').data]
-
-await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-	body: [...slashCommands, ...contextMenuCommands],
-})
