226a5d77cc35445e7b6250fdf97c353e79fceb78
poker-cats-co <hasiradoo@starowl.dev>
2025-07-06 15:56:14 +0200

    Refactor DB setup and update dependencies
    Replaced src/db/generate.ts with src/db/index.ts to initialize the database using drizzle-orm and postgres, loading configuration from environment variables. Updated package.json scripts for development and database management, added new dependencies (@discordjs/rest, dotenv, postgres), and adjusted versions for some packages. Updated bun.lockb to reflect these changes.

--- src/db/generate.ts
+++
@@ -1,4 +0,0 @@
-import { $ } from 'bun'
-
-console.log('bunx drizzle-kit generate')
-await $`bunx drizzle-kit generate`
---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import 'dotenv/config'
+
+if (!process.env.DATABASE_URL) {
+	throw new Error('DATABASE_URL is not set in .env file')
+}
+
+const client = postgres(process.env.DATABASE_URL)
+export const db = drizzle(client)
